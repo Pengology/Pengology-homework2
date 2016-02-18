ae2bbadbf5057c3c18be148521189df3e296fd7a
Shengyu Peng <sp3658@nyu.edu>
2016-02-18 00:08:44 -0500

    After running astyle

--- main.c
+++ main.c
@@ -4,7 +4,7 @@
 #define ROWS 5
 #define COLS 5
 
-int** load_file(FILE* file){};
+int** load_file(FILE* file) {};
 
 int main()
 {
@@ -21,47 +21,52 @@ int main()
     int *rowLargest;
     int *sumCol;
     int t;
-	fptr1 = fopen("twenty-five-ints.txt","r");
-	if(fptr1 == NULL){
-		printf("File not found!\n");
-	}
+    fptr1 = fopen("twenty-five-ints.txt","r");
+    if(fptr1 == NULL)
+    {
+        printf("File not found!\n");
+    }
     arr = load_file(fptr1);
     rowLargest = row_max(arr);
     sumCol = sum_cols(arr);
     printf("The Largest Elements in each row: \n");
-    for ( t = 0; t < ROWS ; i++ ) {
-      printf("*(rowLargest + %d) : %f\n",  t, *(rowLargest + t) );
-   }
-   
-   printf("\nThe Sum of each Column: \n");
-    for ( t = 0; t < COLS ; i++ ) {
-      printf("*(sumCol + %d) : %f\n",  t, *(sumCol + t) );
-   }
-   
-   free(rowLargest);
-   free(sumCol);
+    for ( t = 0; t < ROWS ; i++ )
+    {
+        printf("*(rowLargest + %d) : %f\n",  t, *(rowLargest + t) );
+    }
+
+    printf("\nThe Sum of each Column: \n");
+    for ( t = 0; t < COLS ; i++ )
+    {
+        printf("*(sumCol + %d) : %f\n",  t, *(sumCol + t) );
+    }
+
+    free(rowLargest);
+    free(sumCol);
     return 0;
 }
 
-int** load_file(FILE* file){
+int** load_file(FILE* file)
+{
 
-	int i;
+    int i;
     int* ptr;
-	int j = 0;
-	int** matrix = (int**) malloc(ROWS*sizeof(*int));
-	for(i = 0; i < ROWS; i++){
-		matrix[i] = (int*) malloc(COLS*sizeof(int));
-	}
-	for(i = 0; i < ROWS; i++)
+    int j = 0;
+    int** matrix = (int**) malloc(ROWS*sizeof(*int));
+    for(i = 0; i < ROWS; i++)
     {
-    for(j = 0; j < COLS; j++)
-    {
-        fscanf(fptr1, "%d ", &ptr);
-        matrix[i][j] = num;
+        matrix[i] = (int*) malloc(COLS*sizeof(int));
     }
-}
+    for(i = 0; i < ROWS; i++)
+    {
+        for(j = 0; j < COLS; j++)
+        {
+            fscanf(fptr1, "%d ", &ptr);
+            matrix[i][j] = num;
+        }
+    }
 
-	fclose(fptr1);
-	return matrix;
+    fclose(fptr1);
+    return matrix;
 }
 
--- matrix.c
+++ matrix.c
@@ -1,32 +1,35 @@
 #include "matrix.h"
 
-int* row_max(int** array){
-	int i,j;
-	int* comp = malloc(sizeof(int) * ROWS);
-	for(i = 0; i < ROWS; i++){
-            comp = array[i][0];
-		for(j=0; j< COLS; j++)
+int* row_max(int** array)
+{
+    int i,j;
+    int* comp = malloc(sizeof(int) * ROWS);
+    for(i = 0; i < ROWS; i++)
+    {
+        comp = array[i][0];
+        for(j=0; j< COLS; j++)
         {
             if(comp < array[i][j])
             {
                 comp = array[i][j];
             }
         }
-	}
-	return comp;
+    }
+    return comp;
 }
 
 
-int* sum_cols(int** array){
-    
-        int i,j;
-        int* sum = malloc(sizeof(int) * COLS);
-              //sum of each individual column
-        for (j= 0; j < COL; j++)
-        {
-            sum = 0;
-            for (i = 0; i < ROWS; i++)
-                sum = sum + array[i][j];
-        }
-        return sum;
+int* sum_cols(int** array)
+{
+
+    int i,j;
+    int* sum = malloc(sizeof(int) * COLS);
+    //sum of each individual column
+    for (j= 0; j < COL; j++)
+    {
+        sum = 0;
+        for (i = 0; i < ROWS; i++)
+            sum = sum + array[i][j];
+    }
+    return sum;
 }
